bee74e84e9b7719ce0b2ec79705f3705e06f84c6
owais0017 <owaisshah1717@gmail.com>
2024-01-09 23:13:02 +0530

    added items to cart

--- my-clothing/src/components/menu-item/cart-dropdown/cart-dropdown.component.jsx
+++ my-clothing/src/components/menu-item/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import CustomButton from "../custom-button/custom-button.component";
 import './cart-dropdown.style.scss';
-
-
+import CartItem  from "../cart-item/cart-item.component";
+import { useSelector } from "react-redux";
 
 const CartDropdown = () => {
+  const cartItems = useSelector(state=>state.Cart.cartItem)
   return(
   <div className='cart-dropdown'>
-    <div className='cart-items' />
+    <div className='cart-items' >
+      {
+        cartItems.map(cartitem=>(
+          <CartItem key={cartitem.id} item = {cartitem}/>
+        ))
+      }
+    </div>
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
 
